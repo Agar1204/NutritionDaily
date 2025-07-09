3604b5160bd5e24cc745dc8e460d1a88ee759ef7
Rishi Agarwal <rishiagarwal1217@gmail.com>
2025-07-08 23:08:51 -0700

    added nlp endpoint and displaying result to page

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,10 +1,13 @@
 import Header from "./Header"
-import { Card, Form, Row, Col, ListGroup } from "react-bootstrap"
+import { Card, Form, Row, Col, ListGroup, Badge, Button } from "react-bootstrap"
 
 import { useState, useEffect } from "react"
 export default function Dashboard(){
     const [search, setSearch] = useState("")
     const [searchResults, setSearchResults] = useState([])
+    
+    const [finalSearch, setFinalSearch] = useState("")
+    const [finalSearchResult, setFinalSearchResult] = useState([])
 
     useEffect(() => {
         if(search != ""){
@@ -18,10 +21,39 @@ export default function Dashboard(){
         }
     }, [search])
 
+    useEffect(() => {
+        if(finalSearch != ""){
+            console.log(finalSearch)
+            fetch('https://trackapi.nutritionix.com/v2/natural/nutrients', {
+                method: 'POST',
+                headers: {
+                    'x-app-id': import.meta.env.VITE_NUTRITIONIX_APP_ID,
+                    'x-app-key': import.meta.env.VITE_NUTRITIONIX_API_KEY,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    query: finalSearch
+                })
+            }).then((resp) => resp.json()).then(data => setFinalSearchResult(data.foods)).catch((error) => alert(error.message))
+        }
+    }, [finalSearch])
+
     function handleChange(event){
         setSearch(event.target.value)
     }
 
+    function handleSuggestionClick(data){
+        setFinalSearch(data.food_name)
+        setSearch("")
+    }
+
+    function handleSubmit(event){
+        event.preventDefault()
+        const formData = new FormData(event.target)
+        const searchValue = formData.get('search')
+        setSearch("")
+        setFinalSearch(searchValue)
+    }
     return(
         <div style={{backgroundColor: '#f3fae3'}} className="min-vh-100">
             <Header />
@@ -33,24 +65,56 @@ export default function Dashboard(){
             </Card>
 
             <div className="d-flex justify-content-center px-3">
-                <Form style={{maxWidth: "600px", width: "100%"}}>
+                <Form onSubmit = {handleSubmit} style={{maxWidth: "600px", width: "100%"}}>
                     <Form.Group className="mb-3" id="search">
                         <Form.Label className="text-center w-100 fw-semibold">Search for Foods</Form.Label>
                         <div className="position-relative">
                             <Form.Control 
                                 type="search" 
-                                placeholder="ex: 3 slices of cheese pizza and a can of Diet Coke" 
+                                name="search"
+                                placeholder="ex: 3 eggs" 
                                 value={search}
-                                onChange={handleChange}/>
+                                onChange={handleChange}
+                            />
                             {search.length > 0 && (
                                 <ListGroup className="position-absolute w-100" style={{ top: '100%'}}>
                                     {searchResults.slice(0, 8).map((data, index) => (
-                                        <ListGroup.Item key={index} action> {data.food_name} </ListGroup.Item>))}
+                                        <ListGroup.Item key={index} action onClick={() => handleSuggestionClick(data)}> {data.food_name} </ListGroup.Item>))}
                                 </ListGroup>)}
+
+                            {finalSearchResult.length > 0 && (
+                                <ListGroup className="mt-3">
+                                    {finalSearchResult.map((food) => (
+                                        <ListGroup.Item
+                                            key={food.ndb_no}
+                                            className="d-flex justify-content-between align-items-center"
+                                        >
+                                            <div>
+                                                    <div className="fw-bold">{food.food_name}</div>
+                                                    <small className="text-muted">
+                                                    {food.serving_qty} {food.serving_unit}
+                                                </small>
+                                            </div>
+                                            <Badge bg="success">
+                                                {Math.round(food.nf_calories)} kcal <br />
+                                                {Math.round(food.nf_total_fat)}g of fat <br />
+                                                {Math.round(food.nf_protein)}g of protein <br />
+                                                {Math.round(food.nf_total_carbohydrate)}g of carbohydrates
+                                            </Badge>
+
+                                            <Button 
+                                                variant="secondary" 
+                                                size="sm" 
+                                            > + </Button>
+                                        </ListGroup.Item>
+                                    ))}
+                                </ListGroup>
+                            )}   
+
                         </div>
                         <Form.Text className="text-muted d-block mt-2">
                             Note: You can search using plain English and be as specific/general as you like. 
-                            Including serving sizes and chaining multiple foods is allowed.
+                            Including serving sizes and chaining multiple foods is encouraged.
                         </Form.Text>
                     </Form.Group>
                 </Form>
@@ -64,7 +128,6 @@ export default function Dashboard(){
                     <h1 className="text-center"> Today's Summary </h1>
                 </Col>
             </Row>
-
         </div>
     )
 }
